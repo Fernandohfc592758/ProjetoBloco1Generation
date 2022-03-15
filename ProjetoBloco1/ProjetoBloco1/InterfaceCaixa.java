1ff681d8998acab5005eb065df216d6d41c6e741
Fernandohfc592758 <fernandohfc@outlook.com>
2022-03-15 19:58:44 -0300

    CaixaEletronico

---
+++ ProjetoBloco1/ProjetoBloco1/InterfaceCaixa.java
@@ -0,0 +1,12 @@
+package ProjetoBloco1;
+
+public interface InterfaceCaixa {
+
+	void login();
+	void consultarSaldo();
+	void sacar(double valorDoSaque);
+	void depositar(double deposito);
+	void chequeEspecial();
+	void informarCredito();
+
+}
